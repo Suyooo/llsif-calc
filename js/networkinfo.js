f0225875114d6ddf529812f3ee3798c1f33ef217
Suyooo <superyoshi1@gmx.de>
2019-10-20 08:48:20 +0200

    Fixed empty space below event banner on small screens

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,9 +1,9 @@
-var enDateOverride = [new Date(Date.UTC(2019, 8, 27, 9)),
-    new Date(Date.UTC(2019, 9, 6, 8))];
+var enDateOverride = [new Date(Date.UTC(2019, 9, 14, 9)),
+    new Date(Date.UTC(2019, 9, 22, 8))];
 var jpDateOverride = null;
 
 var notificationInfo = {
-    message: "The <a href=\"/sifas/calc\"><b>SIFAS Event Calculator</b></a> is now available at <a href=\"/sifas/calc\">suyo.be/sifas/calc</a>! The results it calculates won't be correct until a few hours after event start, but feel free to play around with the buttons!<br>Also, <a href=\"https://github.com/Suyooo/llsif-calc/\">this project is now on GitHub</a>! Check the sidebar menu for a link to the Issues page where you can post all of your problems and suggestions for the calculator!",
+    message: "The <a href=\"/sifas/calc\"><b>SIFAS Event Calculator</b></a> is now available at <a href=\"/sifas/calc\">suyo.be/sifas/calc</a>!<br>Also, <a href=\"https://github.com/Suyooo/llsif-calc/\">this project is now on GitHub</a>! Check the sidebar menu for a link to the Issues page where you can post all of your problems and suggestions for the calculator!",
     id: 12,
     allowDismiss: true
 };
@@ -15,7 +15,7 @@ setTimeout(function () {
         showUpdatePrompt();
     } else if (window.applicationCache.status == 1) { // appcache noupdate (didn't recognize removal of the appcache)
         notificationInfo = {
-            message: "<b>WARNING:</b> You are running an outdated version of the calculator, but for some reason, your browser is refusing to update. Please try refreshing and purging your cache. If this message still appears, please contact me (links in the menu). Sorry for the inconvenience!",
+            message: "<b>WARNING:</b> You are running an outdated version of the calculator, but for some reason, your browser is refusing to update. Please try purging your cache and refreshing. If this message still appears, please contact me (links in the menu). Sorry for the inconvenience!",
             id: 999,
             allowDismiss: false
         };
