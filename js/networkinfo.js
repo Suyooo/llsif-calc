88b9db451b580b7d6ca263976155eeffb1878a9c
Suyooo <me@suyo.be>
2021-05-31 12:27:23 +0200

    It's happening

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,24 +1,7 @@
-var enDateOverride = [new Date(Date.UTC(2021, 3, 30, 9)),
-    new Date(Date.UTC(2021, 4, 10, 8))];
-var jpDateOverride = null;
+var dateOverride = null;
 
 var notificationInfo = {
-    message: "<b>HEADS UP:</b> WW didn't add Master difficulty to MedFes. Watch out for that when using estimations - and also watch out for the Cheer Bonus <b>RESETTING EVERY TIME YOU PLAY A MEDLEY OR LEAVE THE EVENT SCREEN SO YOU HAVE TO SET IT AGAIN BEFORE EVERY LIVE</b>. Have fun!",
-    id: 22,
-    allowDismiss: true
+    message: "<b>HEADS UP:</b> WW didn't add Master difficulty to MedFes. Watch out for that when using estimations - and also watch out for a visual bug related to the Cheer Bonus that makes it look like it resets every time you play a medley (it doesn't).",
+    id: 23,
+    allowDismiss: false
 };
-
-// Little prompt for appcache -> service worker migration, will be removed at some point
-// But this file is the only way for me to get scripts in there
-setTimeout(function () {
-    if (window.applicationCache.status == 5) { // appcache obsolete (as in, it was correctly recognized that it's gone)
-        showUpdatePrompt();
-    } else if (window.applicationCache.status == 1) { // appcache noupdate (didn't recognize removal of the appcache)
-        notificationInfo = {
-            message: "<b>WARNING:</b> You are running an outdated version of the calculator, but for some reason, your browser is refusing to update. Please try purging your cache and refreshing. If this message still appears, please contact me (links in the menu). Sorry for the inconvenience!",
-            id: 999,
-            allowDismiss: false
-        };
-        notificationLoad();
-    }
-}, 2000);
