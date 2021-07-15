a10b2a52060066850f33ae84f3a4f384038f9912
Suyooo <me@suyo.be>
2021-07-15 09:22:33 +0200

    Dependency cleanup

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,7 +1,7 @@
 var dateOverride = null;
 
 var notificationInfo = {
-    message: "<b>June 10 Update: 🦀🦀🦀 WW IS GONE 🦀🦀🦀</b><br>Of course, with an update like removing all region selection code, new bugs might crop up. Please let me know if you find any problems (contacts are in the side menu)!",
+    message: "<b>June 8 Update: 🦀🦀🦀 WW IS GONE 🦀🦀🦀</b><br>Of course, with an update like removing all region-based code, new bugs might crop up. Please let me know if you find any problems (contacts are in the side menu)!",
     id: 24,
     allowDismiss: true
 };
